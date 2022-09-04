52e3250dd04dc3630747a9f908f0c6e12d0180e8
Omer Zirh <omerzirh@gmail.com>
2022-09-04 23:48:45 +0200

    add(halftime):implemented in the display score function, shows halftime tag in the score board

--- src/containers/Live/index.js
+++ src/containers/Live/index.js
@@ -11,11 +11,8 @@ import {
   TimeWrapper,
 } from "./style.elements";
 import { scoreContext } from "../../contexts/ScoreContext";
-import { MATCH_DURATION } from "../../helpers/constants";
-import {
-  getGoalIntervals,
-  getTimeStr,
-} from "../../helpers/helper";
+import { HALFTIME_DURATION, MATCH_DURATION } from "../../helpers/constants";
+import { getGoalIntervals, getTimeStr } from "../../helpers/helper";
 
 export default function Index() {
   const [seconds, setSeconds] = useState(0);
@@ -27,21 +24,17 @@ export default function Index() {
   const [awayName, setAwayName] = useState("");
   const [currMatchIndex, setCurrentMatchIndex] = useState(0);
   const [liveActive, setLiveActive] = useState(true);
-  const {
-    nextList,
-    setNextList,
-    setFinishedList,
-    matchesAll,
-    setLiveList,
-  } = useContext(scoreContext);
+  const [halfTime, setHalfTime] = useState(false);
+  const { nextList, setNextList, setFinishedList, matchesAll, setLiveList } =
+    useContext(scoreContext);
 
   //refresh state when a match finished
-  const refreshState = () => {   
+  const refreshState = () => {
     setSeconds(0);
     setHomeScore(0);
     setAwayScore(0);
     setMinutes(0);
-    setCurrentMatchIndex((currMatch) => currMatch+1);
+    setCurrentMatchIndex((currMatch) => currMatch + 1);
   };
 
   //create random goal intervals and make the goals when interval equal to timeleft
@@ -50,16 +43,17 @@ export default function Index() {
     const homeTimeInterval = randomGoalTimes[0];
     const awayTimeInterval = randomGoalTimes[1];
 
+    let countdown = MATCH_DURATION + HALFTIME_DURATION; 
     let timeLeft = MATCH_DURATION;
-
     let timer = setInterval(function () {
-      if (timeLeft === 0) {
+      if (countdown === 0) {
         setNextList((nextList) => nextList.filter((_, index) => index !== 0));
         setFinishedList((current) => [...current, matchesAll[currMatchIndex]]);
         refreshState();
         clearInterval(timer);
         return;
       }
+
       if (MATCH_DURATION - timeLeft === awayTimeInterval.at(-1)) {
         setAwayScore((awayScore) => awayScore + 1);
         awayTimeInterval.pop();
@@ -68,16 +62,26 @@ export default function Index() {
         setHomeScore((homeScore) => homeScore + 1);
         homeTimeInterval.pop();
       }
-
-      setSeconds((seconds) => seconds + 1);
-      timeLeft -= 1;
+      //halftime check
+      if (
+        countdown <= MATCH_DURATION / 2 + HALFTIME_DURATION &&
+        countdown > MATCH_DURATION / 2
+      ) {
+        countdown -= 1;
+        setHalfTime(true);
+      } else {
+        setHalfTime(false);
+        setSeconds((seconds) => seconds + 1);
+        countdown -= 1;
+        timeLeft -= 1;
+      }
     }, 1000);
   };
-//update scoreboard timer and live table timer
+  //update scoreboard timer and live table timer
   useEffect(() => {
     setTimer(getTimeStr(minutes, seconds));
-    if (seconds >=1 )
-     { setLiveList([
+    if (seconds >= 1) {
+      setLiveList([
         {
           key: 1,
           home: homeName,
@@ -86,16 +90,18 @@ export default function Index() {
           awayScore: awayScore,
           status: getTimeStr(minutes, seconds),
         },
-      ]);}
+      ]);
+    }
   }, [seconds]);
 
-  
   useEffect(() => {
-    if (currMatchIndex === matchesAll.length) {  //handle when matches finished
+    if (currMatchIndex === matchesAll.length) {
+      //handle when matches finished
       setLiveActive(false);
       setLiveList([]);
-    } else {                                  //update next match
-      setHomeName(nextList[0].home);    
+    } else {
+      //update next match
+      setHomeName(nextList[0].home);
       setAwayName(nextList[0].away);
       displayScore(
         matchesAll[currMatchIndex].homeScore,
@@ -118,7 +124,7 @@ export default function Index() {
       <TimeWrapper>
         {liveActive ? (
           <>
-            <Label>Live</Label>
+            <Label>{halfTime?<>HALFTIME</>:<>LIVE</>}</Label>
             <TimeLabel>{timer}</TimeLabel>
           </>
         ) : (
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,2 +1,2 @@
-export const MATCH_DURATION = 16; // seconds
+export const MATCH_DURATION = 30; // seconds
 export const HALFTIME_DURATION=5; // seconds
